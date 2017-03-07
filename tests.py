950c5b549b5da52226658ff81062b337e7700afc
Royendgel Silberie <royendgel@gmail.com>
2017-03-06 21:37:02 -0400

    added test skeleton

---
+++ tests.py
@@ -0,0 +1,116 @@
+import unittest
+
+            ("ItemReceipt", 6.0),
+            ("Item", 6.0),
+            ("Estimate", 6.0),
+            ("Class", 6.0),
+            ('Estimate', 6.0),
+            ('Employee', 6.0),
+            ('ItemPayment', 6.0),
+class CustomerTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class AccountTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class InvoiceTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class BillTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class CheckTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class HostTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class ReceivePaymentTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+class ToDoTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class TermsTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class StandardTermsTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class TemplateTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class SalesOrderTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class ItemSalesTaxTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class ItemReceiptTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class ItemTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class EstimateTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class SalesReceiptTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class VendorReceiptTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class ItemPaymentTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class ClassTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+class EmployeeTestCase(unittest.TestCase):
+    def test_add_customer(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
