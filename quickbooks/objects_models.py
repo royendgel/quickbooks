a005400ccfd8655aadd835fa19b784bf2c35da94
Royendgel Silberie <royendgel@gmail.com>
2017-03-13 00:20:48 -0400

    added all add methods

---
+++ quickbooks/objects_models.py
@@ -0,0 +1,156 @@
+BillModel = [
+    ('VendorRef', [
+        'ListID',
+        'FullName',
+    ]),
+    ('VendorAddress', [
+        'Addr1',
+        'Addr2',
+        'Addr3',
+        'Addr4',
+        'Addr5',
+        'City',
+        'State',
+        'PostalCode',
+        'Country',
+        'Note'
+    ]),
+    ('APAccountRef', [
+        'ListID',
+        'FullName'
+
+    ]),
+    'TxnDate',
+    'DueDate',
+    'RefNumber',
+    ('TermsRef', [
+        'ListID',
+        'FullName'
+
+    ]),
+    'Memo',
+    'ExchangeRate',
+    'ExternalGUID',
+    'LinkToTxnID',
+    ('ExpenseLineAdd', [
+        ('AccountRef', [
+            'ListID',
+            'FullName'
+
+        ]),
+        'Amount',
+        'Memo',
+        ('CustomerRef', [
+            'ListID',
+            'FullName'
+
+        ]),
+        ('ClassRef', [
+            'ListID',
+            'FullName'
+
+        ]),
+        'BillableStatus',
+        ('SalesRepRef', [
+            'ListID',
+            'FullName'
+        ]),
+        ('DataExt', [
+            'OwnerID',
+            'DataExtName',
+            'DataExtValue'
+        ])
+    ]),
+    ('ItemLineAdd', [
+        ('ItemRef', [
+            'ListID',
+            'FullName'
+        ]),
+        ('InventorySiteRef', [
+            'ListID',
+            'FullName'
+        ]),
+        ('InventorySiteLocationRef', [
+            'ListID',
+            'FullName'
+        ]),
+        'SerialNumber',
+        'LotNumber',
+        'Desc',
+        'Quantity',
+        'UnitOfMeasure',
+        'Cost',
+        'Amount',
+        ('CustomerRef', [
+            'ListID',
+            'FullName'
+        ]),
+        ('ClassRef', [
+            'ListID',
+            'FullName'
+        ]),
+        'BillableStatus',
+        ('OverrideItemAccountRef', [
+            'ListID',
+            'FullName'
+        ]),
+        ('LinkToTxn', [
+            'TxnID',
+            'TxnLineID'
+        ]),
+        ('SalesRepRef', [
+            'ListID',
+            'FullName'
+        ]),
+        ('DataExt', [
+            'OwnerID',
+            'DataExtName',
+            'DataExtValue'
+        ])
+
+    ]),
+    ('ItemGroupLineAdd', [
+        ('ItemGroupRef', [
+            'ListID',
+            'FullName'
+        ]),
+        'Quantity',
+        'UnitOfMeasure',
+        ('InventorySiteRef', [
+            'ListID',
+            'FullName'
+        ]),
+        ('InventorySiteLocationRef', [
+            'ListID',
+            'FullName'
+        ]),
+        ('DataExt', [
+            'OwnerID',
+            'DataExtName',
+            'DataExtValue'
+
+        ])
+
+    ])
+
+]
+
+CustomerModel = [
+    'Name',
+    'IsActive',
+    ('ParentRef', [
+        'ListID',
+        'FullName'
+    ]),
+    'AccountType',
+    'AccountNumber',
+    'BankNumber',
+    'Desc',
+    'OpenBalance',
+    'OpenBalanceDate',
+    'TaxLineID',
+    ('CurrencyRef', [
+        'ListID',
+        'FullName'
+    ]),
+]
