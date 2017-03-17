996e06ca77406ecae96507e407d60aa03cf51f18
Royendgel Silberie <royendgel@gmail.com>
2017-03-17 12:11:47 -0400

    tests generated

---
+++ tests/generated_tests.py
@@ -0,0 +1,890 @@
+# Automatically generated from script.
+
+import unittest
+
+
+class EntityTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class ItemReceiptTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class AccountTaxLineInfoTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class VehicleTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class DateDrivenTermsTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class TransferTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class TxnDeletedTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class CreditCardCreditTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class CreditCardChargeTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class BarCodeTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class ReceivePaymentToDepositTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class SalesReceiptTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class DataExtTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+
+class StandardTermsTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class TimeTrackingTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class GeneralSummaryReportTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class CustomerTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class SalesTaxReturnTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class ItemOtherChargeTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class JournalEntryTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class AccountTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ItemServiceTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class SalesRepTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ItemDiscountTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ItemSitesTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class PreferencesTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class ItemTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class ChargeTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ItemSalesTaxGroupTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class DataEventRecoveryInfoTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class BillToPayTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class EstimateTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class SalesTaxPaymentCheckTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class PayrollItemNonWageTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class ToDoTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class CheckTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class JobTypeTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ItemInventoryTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class PurchaseOrderTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class VendorTypeTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class HostTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class VendorTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class TransferInventoryTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class UnitOfMeasureSetTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class VehicleMileageTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ListDeletedTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class ShipMethodTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class PayrollLastPeriodTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class BudgetSummaryReportTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class ItemNonInventoryTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class SpecialAccountTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+
+class SpecialItemTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+
+class CompanyTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class PayrollItemWageTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class CreditMemoTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class PriceLevelTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class BillPaymentCreditCardTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class BillingRateTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class InventoryAdjustmentTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ClassTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class CustomerMsgTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class DepositTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ItemAssembliesCanBuildTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class TermsTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class OtherNameTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ARRefundCreditCardTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class JobReportTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class ItemFixedAssetTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ItemSubtotalTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class EmployeeTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class GeneralDetailReportTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class PayrollSummaryReportTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class InvoiceTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class AgingReportTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class SalesOrderTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class CustomDetailReportTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class PayrollDetailReportTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class DataExtDefTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class VendorCreditTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class TimeReportTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class BuildAssemblyTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class CustomerTypeTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ItemGroupTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class TemplateTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class ItemSalesTaxTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class CompanyActivityTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class ListDisplayTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+
+class WorkersCompCodeTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ReceivePaymentTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class BillTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ItemInventoryAssemblyTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class CurrencyTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ItemPaymentTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class SalesTaxReturnLineTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class Form1099CategoryAccountMappingTestCase(unittest.TestCase):
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class BillPaymentCheckTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class TxnDisplayTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+
+class PaymentMethodTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class ClearedStatusTestCase(unittest.TestCase):
+    def test_mod(self):
+        pass
+
+
+class InventorySiteTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class TransactionTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class CustomSummaryReportTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
+class SalesTaxCodeTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class LeadTestCase(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_mod(self):
+        pass
+
+    def test_query(self):
+        pass
+
+
+class SalesTaxPayableTestCase(unittest.TestCase):
+    def test_query(self):
+        pass
+
+
